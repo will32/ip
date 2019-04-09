95841ebbf2cd618089a05e8af027e967a6352853
William Lin <linzhijun032@gmail.com>
2019-04-09 16:15:23 -0700

    fix service response

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send(req.ip))
+app.get('/', (req, res) => {
+    let { ip } = req;
+    if (ip.startsWith("::ffff:")) {
+        // remove ipv4 signature
+        ip = ip.slice(7);
+    }
+    res.send(ip);
+    return;
+})
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
